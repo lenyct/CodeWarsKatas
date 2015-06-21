cdf57660b220c0ddaede281145cdbea9117bf53b
Paul leonard <Paul.leonard88@hotmail.com>
2015-06-21 15:41:55 +0100

    inital Commit

---
+++ CodeWarsKatas/CodeWarsKatas/Katas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKatas
+{
+    public class Katas
+    {
+      // return masked string
+      public static string Mask(string cc)
+      {
+          return cc.Length > 4 ? cc.Substring(cc.Length - 4).PadLeft(cc.Length, '#') : cc;
+      }
+
+    }
+}
---
+++ CodeWarsKatas/KataTesting/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace KataTesting
+{
+    [TestFixture]
+    class Tests
+    {
+            [Test]
+            public void ExamplesTests()
+            {
+                Assert.AreEqual(CodeWarsKatas.Katas.Mask("4556364607935616"), "############5616");
+                Assert.AreEqual(CodeWarsKatas.Katas.Mask("1"), "1");
+                Assert.AreEqual(CodeWarsKatas.Katas.Mask("11111"), "#1111");
+            }
+
+        
+    }
+}
