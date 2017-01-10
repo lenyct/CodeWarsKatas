6fd4574f2acd3d68600722ff168087f6868bf46e
paul Leonard <paul_Leonard88@hotmail.com>
2017-01-10 09:38:50 +0000

    final tidy after genva using statements edit

--- CodeWarsKatas/CodeWarsKatas/Katas2.cs
+++ CodeWarsKatas/CodeWarsKatas/Katas2.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Numerics;
-using System.Text.RegularExpressions;
 
 namespace CodeWarsKatas
 {
