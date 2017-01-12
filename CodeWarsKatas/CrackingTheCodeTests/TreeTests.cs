666e1e9cf2d9cf0868650064138553885e5d5928
paul Leonard <paul_Leonard88@hotmail.com>
2017-01-12 14:17:46 +0000

    add missing files

--- CodeWarsKatas/CrackingTheCodeTests/TreeTests.cs
+++ CodeWarsKatas/CrackingTheCodeTests/TreeTests.cs
@@ -29,12 +29,14 @@ namespace CrackingTheCodeTests
         public void TestBalancedTree()
         {
             TreeNode leaf1 = new TreeNode(null, null);
-            TreeNode leaf2 = new TreeNode(null, null);            
-            TreeNode node2 = new TreeNode(leaf1, leaf2);            
-            TreeNode node = new TreeNode(node2  , node2);
+            TreeNode leaf2 = new TreeNode(null, null);
+            TreeNode leaf3 = new TreeNode(null, null);
+            TreeNode node2 = new TreeNode(leaf1, null);
+            TreeNode node3 = new TreeNode(leaf1, null);
+            TreeNode node = new TreeNode(node2, null);
             TreeNode root = new TreeNode(node, node);
 
-            Assert.AreEqual(true, TreeTraversal.IsBalanced(root));
+            Assert.AreEqual(4, TreeTraversal.GetHeight(root));
         }
     }
 }
